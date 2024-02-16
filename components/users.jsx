145a9df9c5d72bd18b47048ce853f27b2826d7b3
mateoleont <mateolt505@gmail.com>
2024-02-16 00:10:46 -0500

    id para users

--- components/users.jsx
+++ components/users.jsx
@@ -1,24 +1,21 @@
 'use client'
+import Link from 'next/link'
 import React from 'react'
 
 const Users = ({users}) => {
 
-    console.log(users);
-
-    const handleOnClick = () => {
-
-    }
-
   return (
     <ul>
     {users.map((user) =>(
-      <li key={user.id} className='bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between'>
-        <div>
-          <h5 className='font-bold'>{user.id}. {user.first_name} {user.last_name}</h5>
-          <p className='text-slate-50'>Email: {user.email}</p>              
-        </div>
-        <img src={user.avatar} className='rounded-full w-20' alt={`${user.first_name} ${user.last_name}`}/>
-      </li>
+        <Link href={`/users/${user.id}`}  key={user.id}>
+            <li  className='bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between'>
+                <div>
+                    <h5 className='font-bold'>{user.id}. {user.first_name} {user.last_name}</h5>
+                    <p className='text-slate-50'>Email: {user.email}</p>              
+                </div>
+                <img src={user.avatar} className='rounded-full w-20' alt={`${user.first_name} ${user.last_name}`}/>
+            </li>
+        </Link>
     ))}
   </ul>
   )
